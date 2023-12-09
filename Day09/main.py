a7e2e5c59530f0bef9efb055c104dfcb475caf49
Joe Pitts <JPitts@TrueNorthIT.co.uk>
2023-12-09 14:07:40 +0000

    Refactored

--- Day09/main.py
+++ Day09/main.py
@@ -1,62 +1,25 @@
+from functools import reduce
+
 def read_input(filename: str = "input.txt"):
     with open(filename, "r") as f: 
         return [list(map(int, l.split(" "))) for l in f.readlines()]
     
-
-def difference_at_step(reading: list[int]):
-    # 0 3 6 9 12 15 -> 3 3 3 3 3
-    # 3 3 3 3 3 -> 0 0 0 0
-
-    difference = [ reading[i+1] - reading[i] for i in range(len(reading) - 1) ]
-    return difference
-
-def extrapolate(difference: int, lastInSeq: int):
-    return lastInSeq + difference
-
-def extrapolate2(difference: int, firstInSeq: int):
-    return firstInSeq - difference
-
-
-def part1(oasis: list[str]):
-    extrapolatedSum = 0
+def solve(oasis: list[str]):
+    def difference_at_step(reading: list[int]) -> list[int]:
+        return [b-a for a,b in zip(reading, reading[1:])]    
+    
+    part1, part2 = 0, 0
     for reading in oasis:
-        print()
-        differences = [reading]
-        last_difference = difference_at_step(reading)
-        differences.append(last_difference)
-        while [a for a in last_difference if a != 0]:
-            last_difference = difference_at_step(last_difference)
-            differences.append(last_difference)
-            
-        differenceToMatch = 0
-        for diff in differences[::-1]:
-            oldDifferenceToMatch = differenceToMatch
-            differenceToMatch = extrapolate(differenceToMatch, diff[-1])
-            print(diff, oldDifferenceToMatch, differenceToMatch)
-        extrapolatedSum += differenceToMatch
-    return extrapolatedSum
+        differences = [reading, difference_at_step(reading)]
+        while not all(a == 0 for a in differences[-1]):
+            differences.append(difference_at_step(differences[-1]))
+        part1 += reduce(lambda acc, lst: lst[-1] + acc, reversed(differences), 0)
+        part2 += reduce(lambda acc, lst: lst[0] - acc , reversed(differences), 0)
 
-def part2(oasis: list[str]):
-    extrapolatedSum = 0
-    for reading in oasis:
-        differences = [reading]
-        last_difference = difference_at_step(reading)
-        differences.append(last_difference)
-        while [a for a in last_difference if a != 0]:
-            last_difference = difference_at_step(last_difference)
-            differences.append(last_difference)
-            
-        differenceToMatch = 0
-        for diff in differences[::-1]:
-            differenceToMatch = extrapolate2(differenceToMatch, diff[0])
-        extrapolatedSum += differenceToMatch
-    return extrapolatedSum
-
-
+    return part1, part2
 
 if __name__ == "__main__":
     readings = read_input()
-    # print(part1(readings))
-    print()
-    print(part2(readings))
-
+    part1, part2 = solve(readings)
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
