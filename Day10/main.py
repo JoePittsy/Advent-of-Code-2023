02eeca06c7ba389e18c96cee2603529e88b1f88c
Joe Pitts <JPitts@TrueNorthIT.co.uk>
2023-12-11 09:14:14 +0000

    Refactored

--- Day10/main.py
+++ Day10/main.py
@@ -1,4 +1,5 @@
 import numpy as np 
+from matplotlib.path import Path
 
 def read_input(filename: str = "input.txt"):
     with open(filename, "r") as f:
@@ -11,72 +12,24 @@ def read_input(filename: str = "input.txt"):
 def check_neighbors(grid, my, mx):
     my_location = grid[my][mx] # Y and X are flipped :) 
     def get_adjacents(grid, x, y):
-        adjacents = [(-1, 0), (0, -1), (0, 1), (1, 0)]  # Up, Left, Right, Down
-        return [(x + dx, y + dy) for dx, dy in adjacents if 0 <= x + dx < len(grid) and 0 <= y + dy < len(grid[0])]
-
-    # Define connections based on direction
+        directions = {
+            (-1, 0): "left",
+            (0, -1): "up",
+            (0, 1): "down",
+            (1, 0): "right"
+        }
+        return [((x + dx, y + dy), directions[(dx, dy)]) for dx, dy in directions if 0 <= x + dx < len(grid) and 0 <= y + dy < len(grid[0])]
 
     connections = {
-        "┌": {
-            "up": [],
-            "down": ["│", "└", "┘"],
-            "left": [],
-            "right": ["─", "┐", "┘"]
-        },
-        "└": {
-            "up": ["│", "┌", "┐"],
-            "down": [],
-            "left": [],
-            "right": ["─", "┘", "┐"]
-        },
-        "┐": {
-            "up": [],
-            "down": ["│", "┘", "└"],
-            "left": ["─", "└", "┌"],
-            "right": []
-        },
-        "┘": {
-            "up": ["│", "┌", "┐"],
-            "down": [],
-            "left": ["─", "└", "┌"],
-            "right": []
-        },
-        "─": {
-            "up": [],
-            "down": [],
-            "left": ["┌", "┘", "└", "┐", "─"],
-            "right": ["┌", "┘", "└", "┐", "─"]
-        },
-        "│": {
-            "up": ["┌", "│", "┐"],
-            "down": ["┘", "└", "│"],
-            "left": [],
-            "right": []
-        }
+        "┌": { "up": [], "down": ["│", "└", "┘"], "left": [], "right": ["─", "┐", "┘"] },
+        "└": { "up": ["│", "┌", "┐"], "down": [], "left": [], "right": ["─", "┘", "┐"] },
+        "┐": { "up": [], "down": ["│", "┘", "└"], "left": ["─", "└", "┌"], "right": [] },
+        "┘": { "up": ["│", "┌", "┐"], "down": [], "left": ["─", "└", "┌"], "right": [] },
+        "─": { "up": [], "down": [], "left": ["┌", "┘", "└", "┐", "─"], "right": ["┌", "┘", "└", "┐", "─"] },
+        "│": { "up": ["┌", "│", "┐"], "down": ["┘", "└", "│"], "left": [], "right": [] }
     }
 
-    my_connections = connections[my_location]
-
-    adjacents = get_adjacents(grid, mx, my)
-    neighbors = []
-    for x, y in adjacents:
-        direction = ""
-        if y > my:
-            direction = "down"
-        elif y < my:
-            direction = "up"
-        elif x > mx:
-            direction = "right"
-        elif x < mx:
-            direction = "left"
-        
-        neighbor = grid[y][x]
-
-
-        if neighbor in my_connections[direction]:
-            neighbors.append((y, x))
-
-    return neighbors
+    return {(y, x) for (x, y), direction in get_adjacents(grid, mx, my) if grid[y][x] in connections[my_location][direction]}
 
 def fix_s_pipe(G, r, c):
 
@@ -97,47 +50,54 @@ def fix_s_pipe(G, r, c):
     elif left_valid and right_valid:
         G[r][c]='─'
 
+
 def part1(grid):
-    s_location = np.argwhere(grid == "S")
-    fix_s_pipe(grid, s_location[0][0], s_location[0][1])
+    (start_y,), (start_x,) = np.where(grid == "S")
 
-    start_neighbors = check_neighbors(grid, s_location[0][0], s_location[0][1])
-    visited = [start_neighbors[0]]
-    my_location = (s_location[0][0], s_location[0][1])
+    # Initial setup
+    fix_s_pipe(grid, start_y, start_x)
+    my_location = (start_y, start_x)
 
-    steps = 1
+    # Set for visited coordinates
+    visited = {my_location}
+
     while True:
-        steps += 1
-        visited.append(my_location)
+        my_neighbours = check_neighbors(grid, *my_location)
+        my_unvisited_neighbors = my_neighbours - visited
+        if not my_unvisited_neighbors:
+            break  # No unvisited neighbors left, exit the loop
+        my_location = my_unvisited_neighbors.pop()
+        visited.add(my_location)
 
-        my_neighbours = check_neighbors(grid, my_location[0], my_location[1])
-        my_unvisited_neighbors = [n for n in my_neighbours if n not in visited]
-        if len(my_unvisited_neighbors) == 0:
-            break
-        my_location = my_unvisited_neighbors[0]
-        # Remove visited
+    steps = len(visited) // 2
+    return steps, list(visited)
 
 
 
-    print(steps)
-    return (steps / 2), visited
 
-def part2(grid, loop):
-    from matplotlib.path import Path
-
+def part2(loop):
     path = Path(loop)
-    ans = 0
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if (x, y) in loop:
-                continue
-            if path.contains_point((x, y)):
-                ans += 1
-    print(ans)
 
+    min_x, min_y, max_x, max_y = path.get_extents().bounds
+
+    # Create a grid of points within the bounding box of the loop, does not matter the pipe there so we don't even need the grid
+    y_indices, x_indices = np.mgrid[int(min_y):int(max_y) + 1, int(min_x):int(max_x) + 1]
+    all_points_within_bbox = np.vstack([x_indices.ravel(), y_indices.ravel()]).T
+
+    # Filter out the points that are part of the loop
+    loop_set = set(map(tuple, loop))  # Convert loop to a set for faster lookup
+    bounded_points_not_part_of_loop = np.array([pt for pt in all_points_within_bbox if tuple(pt) not in loop_set])
+
+    # Maybe it's a bit of a cheat to use the contains_points method, but it's fast and it works :)
+    # I did implement a ray casting algorithm myself, but it was pretty damn slow. 
+    # interesting how it works, if the ray intersects an odd number of times, the point is inside the loop 
+    points_inside_loop = path.contains_points(bounded_points_not_part_of_loop)
+    ans = np.sum(points_inside_loop)
+
+    return ans
 
 if __name__ == "__main__":  
     data = read_input()
     p1, loop = part1(data)
     print(f"Part 1: {p1}")
-    part2(data, loop)
+    print(f"Part 2: {part2(loop)}")
